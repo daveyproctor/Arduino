d958a9b8d1ad8833424d2ce25481b6d38bb8393b
Davey Proctor <david.proctor@yale.edu>
2019-02-06 21:29:06 -0500

    proof of concept on the second sketch. light blinks using shift reg and led display

--- sketch_part2/sketch_part2.ino
+++ sketch_part2/sketch_part2.ino
@@ -1,10 +1,40 @@
+int latchPin = 12;
+int clockPin = 11;
+int dataPin = 13;
+byte leds = 0;
+int currentLED = 0;
+
 void setup()
 {
-  Serial.begin(9600);
-  
+    Serial.begin(9600);
+    Serial.println("HI");
+    pinMode(latchPin, OUTPUT);
+    pinMode(dataPin, OUTPUT);
+    pinMode(clockPin, OUTPUT);
+
+    leds = 0;
 }
 
-loop()
+void loop()
 {
-  
+    
+    // leds = 0;
+
+    if (leds == 7)
+    {
+        leds = 0;
+    }
+    else
+    {
+        leds++;
+    }
+
+    // bitSet(leds, currentLED);
+
+    Serial.println(leds);
+    digitalWrite(latchPin, LOW);
+    shiftOut(dataPin, clockPin, LSBFIRST, leds);
+    digitalWrite(latchPin, HIGH);
+
+    delay(250);
 }
