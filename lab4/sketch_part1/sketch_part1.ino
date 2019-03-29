f61c475b33223ab2d356b56c81cb9993fc797d08
Davey Proctor <david.proctor@yale.edu>
2019-03-28 22:27:30 -0400

    cleaned up sketch

--- lab4/sketch_part1/sketch_part1.ino
+++ lab4/sketch_part1/sketch_part1.ino
@@ -1,91 +1,48 @@
 #include "concurrency.h"
 
+void p0 (void)
+{
+    /* process 0 here */
+    while(1) {
+        digitalWrite(GREEN, HIGH);
+    }
+}
+
 void p1 (void)
 {
-  return;
-  /* process 1 here */
-  int i = 0;
-  int l = 0;
-  int j = 0;
-  // asm volatile ("cli\n\t");
-  while(1){
-    // Serial.println("-p1 Hi from p1");
-    digitalWrite(YELLOW, HIGH);
-    // continue;
-    // digitalWrite(YELLOW, 1);
-    // delay(1000);
-    // digitalWrite(YELLOW, 0);
-    // delay(1000);
-    continue;
-
-    i++;
-    if (i > 10000){
-      i = 0;
-      l++;
-      if (l > 10000){
-        i = 0;
-        l = 0;
-        // j = 1-j;
+    /* process 1 here */
+    while(1) {
         digitalWrite(YELLOW, HIGH);
-        digitalWrite(YELLOW, LOW);
-      }
     }
-  }
-  return;
- }
+}
 
 void p2 (void)
 {
-  return;
-  /* process 2 here */
-  int i = 0;
-  int j = 0;
-  while(1){
-    // Serial.println("-p2 Hi from p2");
-    digitalWrite(BLUE, 1);
-    // delay(1000);
-    // digitalWrite(BLUE, 0);
-    // delay(1000);
-    continue;
-
-    i++;
-    if (i > 10000){
-      i = 0;
-      j = 1-j;
-      digitalWrite(BLUE, j);
+    /* process 2 here */
+    while(1) {
+        digitalWrite(BLUE, HIGH);
     }
-  }
- return;
 }
 
 void setup()
 {
   Serial.begin(9600);
   Serial.println("Setup");
-  Serial.println(sizeof(int));
-  Serial.println(HIGH);
-  Serial.println(LOW);
   delay(1000);
+
   pinMode(BLUE, OUTPUT);
   pinMode(YELLOW, OUTPUT);
   pinMode(GREEN, OUTPUT);
 
   digitalWrite(GREEN, HIGH);
-  delay(200);
+  delay(500);
   digitalWrite(GREEN, LOW);
-  delay(200);
-  // digitalWrite(6, 1);
-  // delay(1000);
-  // digitalWrite(6, 0);
-  // delay(1000);
-  // digitalWrite(8, 1);
-  // delay(1000);
-  // digitalWrite(8, 0);
-  // delay(1000);
+  delay(500);
 
-  // p1();
-  // p2();
-
+  if (process_create (p0, 64) < 0) {
+    Serial.println("FAIL");
+    return;
+  }
   if (process_create (p1, 64) < 0) {
     Serial.println("FAIL");
     return;
@@ -95,35 +52,16 @@ void setup()
     return;
   }
   process_start();
-//   asm volatile ("sei\n\t"); // REQUIRED, WOW! But only once.
-  // WHAT WE KNOW:
-  // returns once from process_start with interrupts disabled.
-  // returns 5 more times into the loop, interrupts enabled.
-  // Then functions properly.
-
-  // Serial.println("oops");
 }
 
 void loop()
 {
-  // Serial.println("oops");
   /* if you get here, then all processes are either finished or
      there is deadlock */
+  Serial.println("oops");
   int i=0;
   while (1){
-    Serial.println(++i);           // -- exactly 5 times
-    // digitalWrite(GREEN, HIGH);  // -- yes
-    // digitalWrite(GREEN, LOW);   // -- no, only flash, so loop not iterating.
-    // asm volatile ("sei\n\t");   // not necessary
+    Serial.println(++i);
   };
 }
 
-
-
-// void setup(){
-//   Serial.begin(9600);
-// }
-
-// void loop(){
-//   Serial.println("hi");
-// }
