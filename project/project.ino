f0626f50ecd2601a99c8268a38e664a6dc6cb1d3
MichaelMac6072 <47404978+MichaelMac6072@users.noreply.github.com>
2019-04-24 19:43:46 -0400

    Update project.ino

--- project/project.ino
+++ project/project.ino
@@ -8,8 +8,8 @@
 #define MIC_PIN A0
 #define MIC_TRIGGER 10
 #define SPEAKER_PIN 11
-#define A_PIN 6
-#define A_SHARP_PIN 5
+#define A_PIN 5
+#define A_SHARP_PIN 6
 #define F_PIN 8
 #define C_PIN 7
 #define RED_PIN 4
@@ -18,20 +18,20 @@
 
 // Define how lenient this tuner will be
 
-#define CENTS_RANGE 3
+#define CENTS_RANGE 4
 
 // Setting up Fast Fourier Transform Parameters
 
 #define SAMPLES 128 // Must be a power of 2
-#define SAMPLING_FREQ 3400 // Must be less than 1000 due to ADC limitations
+#define SAMPLING_FREQ 2048 // Must be less than 1000 due to ADC limitations
 
 unsigned int sampling_period_us;
 unsigned long microseconds;
 
 double waveReal[SAMPLES];
 double waveImag[SAMPLES];
-double peak;
-double peak_last = 0;
+float peak;
+float peak_last = 0;
 int octave;
 
 boolean a_trig = 0;
@@ -50,120 +50,66 @@ arduinoFFT FFT = arduinoFFT();
         
     No tuning is going to be perfect,
     so we define an acceptable range of in-tune frequencies
-    to be within 1 cent of the center pitch.
+    to be within 4 cents of the center pitch.
     
     We also define what ranges of pitch to detect as "within a family"
     as being 50 cents away from the center pitch.
 */
 
 /*
-  Our base tuning frequencies are defined according to the table at this URL:
-  https://pages.mtu.edu/~suits/notefreqs.html
-  
-  Pitch definitions in terms of Hertz: A to G#
-  More precisely, each pitch is a factor of 2^(1/12)
-  above the lower pitch.
-  A: 440   A#: 466.16   B: 493.88  C: 523.25
-  C#: 554.37  D: 587.33  D#: 622.25  E: 659.25
-  F: 698.46  F#: 739.99  G: 783.99  G#: 830.61
+  Initialize arrays for the in-tone frequencies
+  and the names associated with those notes.
 */
 
 double twelveToneScale[12];
 String twelveToneNames[12] = {"A ", "A# ", "B ", "C ", "C# ", "D ", "D# ", "E ", "F ", "F# ", "G ", "G# "};
+
 /*
-twelveToneNames[0] = String("A ");
-twelveToneNames[1] = String("A# ");
-twelveToneNames[2] = String("B ");
-twelveToneNames[3] = String("C ");
-twelveToneNames[4] = String("C# "); 
-twelveToneNames[5] = String("D ");
-twelveToneNames[6] = String("D# ");
-twelveToneNames[7] = String("E ");
-twelveToneNames[8] = String("F ");
-twelveToneNames[9] = String("F# ");
-twelveToneNames[10] = String("G ");
-twelveToneNames[11] = String("G# ");
-/*
-typedef struct pitchFamily pitchFamilyT;
-struct pitchFamily
-{
-    int pitchTag;
-    double centerPitch;
-    //string serialLabel; // What to print into the Serial Monitor.
-};
+  These functions give us the pitch ranges
+  necessary for classificaiton and checking intonation.
 */
 
-double topPitch(double, int);
-double goodEnoughTop(double, int);
-double bottomPitch(double, int);
-double goodEnoughBottom(double,int);
-double centerPitch(double, int);
+float topPitch(float, int);
+float goodEnoughTop(float, int);
+float bottomPitch(float, int);
+float goodEnoughBottom(float,int);
+float centerPitch(float, int);
 
-/* 
- * Orchestrally speaking,
- * tuning notes are typically A, A#, F, or C, 
- * so we probably only need buttons to play those four. 
- */
-
-void play_a()
-{
-  a_trig = 1;
-  //Serial.println("Interrupt A");
-  delay(200);
-}
-
-void play_asharp()
-{
-  asharp_trig = 1;
-  delay(200);
-}
-
-void play_c()
-{
-  c_trig = 1;
-  delay(200);
-}
-
-void play_f()
-{
-  f_trig = 1;
-  delay(200);
-}
-
-double centerPitch(double note, int octave)
+float centerPitch(float note, int octave)
 {
   return note*pow(2,(float)octave);
 }
 
-double topPitch(double note, int octave){
+float topPitch(float note, int octave){
   return note*pow(2,((float)1/24) + (float)octave);
 }
 
-double goodEnoughTop(double note, int octave){
+float goodEnoughTop(float note, int octave){
   return note*pow(2,((float)CENTS_RANGE/1200) + (float)octave);
 }
 
-double bottomPitch(double note, int octave){
+float bottomPitch(float note, int octave){
   return note*pow(2,(((float)-1/24) + (float)octave));
 }
 
-double goodEnoughBottom(double note, int octave){
+float goodEnoughBottom(float note, int octave){
   return note*pow(2,(((float)-CENTS_RANGE/1200) + (float)octave));
 }
 
 void setup()
 {
-for(int i=0;i<12;i++)
-{
-    twelveToneScale[i] = 440*pow(2,((float)i/12));
-}
+  // Define the center pitches.
+  for(int i=0;i<12;i++)
+  {
+      twelveToneScale[i] = 440*pow(2,((float)i/12));
+  }
 
   Serial.begin(9600);
 
   // Define inputs...
   pinMode(MIC_PIN, INPUT);
   pinMode(MIC_TRIGGER, INPUT);
-  pinMode(A_PIN, INPUT);
+  pinMode(A_PIN, INPUT_PULLUP);
   pinMode(A_SHARP_PIN, INPUT_PULLUP);
   pinMode(C_PIN, INPUT_PULLUP);
   pinMode(F_PIN, INPUT_PULLUP);
@@ -173,72 +119,69 @@ for(int i=0;i<12;i++)
   pinMode(GREEN_PIN, OUTPUT);
   pinMode(BLUE_PIN, OUTPUT);
   
-  //Interrupt setup.
-  attachInterrupt(digitalPinToInterrupt(A_SHARP_PIN),play_asharp,FALLING);
-  attachInterrupt(digitalPinToInterrupt(A_PIN),play_a,FALLING);
-  attachInterrupt(digitalPinToInterrupt(F_PIN),play_f,FALLING);
-  attachInterrupt(digitalPinToInterrupt(C_PIN),play_c,FALLING);
 
   //FFT sampling period setup.
   sampling_period_us = round(1000000*(1.0/SAMPLING_FREQ));
 }
 
 void loop() {
-
-/*
-  if(digitalRead(A_PIN) == LOW)
-  {
-    Serial.println("Intterupt A");
-  }
-*/
-
-  // Reference Pitches
+  // Toggling Reference Pitches.
+  // Done using a polling method of gauging inputs.
 
   // Play A
   
-  while((digitalRead(A_PIN) == LOW) && (asharp_trig = 1)){
-  //Serial.println("LOW");
-  //digitalWrite(12, HIGH);
+  while((digitalRead(A_PIN) == LOW)){
   digitalWrite(RED_PIN, LOW);
   digitalWrite(GREEN_PIN, LOW);
   digitalWrite(BLUE_PIN, LOW);
     tone(SPEAKER_PIN, twelveToneScale[0]); //
   } 
-  a_trig = 0;
+  if(digitalRead(A_PIN == HIGH)){
+  noTone(SPEAKER_PIN);
+  }
 
   //Play A#
-  while((digitalRead(A_SHARP_PIN) == LOW) && (asharp_trig = 1)){
+  while(digitalRead(A_SHARP_PIN) == LOW){
   digitalWrite(RED_PIN, LOW);
   digitalWrite(GREEN_PIN, LOW);
   digitalWrite(BLUE_PIN, LOW);
     tone(SPEAKER_PIN, twelveToneScale[1]);
   }
-  asharp_trig = 0; 
+  if(digitalRead(A_SHARP_PIN == HIGH)){
+  noTone(SPEAKER_PIN);
+  } 
 
   //Play C
-  while((digitalRead(C_PIN) == LOW) && (c_trig = 1)){
+  while(digitalRead(C_PIN) == LOW){
   digitalWrite(RED_PIN, LOW);
   digitalWrite(GREEN_PIN, LOW);
   digitalWrite(BLUE_PIN, LOW);
     tone(SPEAKER_PIN, twelveToneScale[3]);
   } 
-  c_trig = 0;
+  if(digitalRead(A_SHARP_PIN == HIGH)){
+  noTone(SPEAKER_PIN);
+  }
 
   //Play F
-  while((digitalRead(C_PIN) == LOW) && (f_trig = 1)){
+  while(digitalRead(F_PIN) == LOW){
   digitalWrite(RED_PIN, LOW);
   digitalWrite(GREEN_PIN, LOW);
   digitalWrite(BLUE_PIN, LOW);
   tone(SPEAKER_PIN, twelveToneScale[8]);
   } 
-  f_trig = 0;
+  if(digitalRead(A_SHARP_PIN == HIGH)){
+  noTone(SPEAKER_PIN);
+  }
     
   
   //Reading in a wave to tune.
+  /*
+    A guide for our FFT implementation can be found here: 
+    https://www.norwegiancreations.com/2017/08/what-is-fft-and-how-can-you-implement-it-on-an-arduino/
+  */
   
-  while((digitalRead(MIC_TRIGGER) == 1) && (f_trig == 0) && (c_trig == 0) && (asharp_trig == 0) && (a_trig) == 0)
-  {
-    tone(SPEAKER_PIN, 698.46);
+  while((digitalRead(MIC_TRIGGER) == 1) && (digitalRead(A_PIN) == HIGH) && (digitalRead(A_SHARP_PIN) == HIGH) && (digitalRead(C_PIN) == HIGH) && (digitalRead(F_PIN) == HIGH)) // && (f_trig == 0) && (c_trig == 0) && (asharp_trig == 0) && (a_trig == 0))
+  { 
       for(int i=0; i<SAMPLES; i++)
       {
           microseconds = micros();    //Overflows after around 70 minutes!
@@ -255,8 +198,9 @@ void loop() {
       FFT.Compute(waveReal, waveImag, SAMPLES, FFT_FORWARD);
       FFT.ComplexToMagnitude(waveReal, waveImag, SAMPLES);
       peak = FFT.MajorPeak(waveReal, SAMPLES, SAMPLING_FREQ);
-      peak = peak - peak*29/440; // Correction factor
-      //peak = 400;
+      // The limited memory of the Arduino means there will be some error in the FFT.
+      // Corrections need to be made.
+      peak = peak - peak*((double)9/440);
       /*
       Serial.println(peak);
       Serial.println(topPitch(peak, 0));
@@ -281,7 +225,7 @@ void loop() {
                 
                 if((peak >= goodEnoughBottom(twelveToneScale[j], octave)) && (peak <= goodEnoughTop(twelveToneScale[j], octave))) 
                 {
-                  Serial.println("In tune");
+                  Serial.print ("In tune");
                   digitalWrite(RED_PIN, LOW);
                   digitalWrite(GREEN_PIN, HIGH);
                   digitalWrite(BLUE_PIN, LOW);
@@ -303,11 +247,13 @@ void loop() {
                   digitalWrite(BLUE_PIN, HIGH);
                 }
 
-                if(peak != peak_last)
+                if(peak != peak_last) // If the dominant frequency changes...
                 {
                 Serial.print(twelveToneNames[j]);
-                //Serial.println(1200*log(peak/centerPitch(twelveToneScale[j], octave))/log(2));
-                Serial.println(peak);
+                // This prints cents from center
+                //Serial.println(1200*log(peak/centerPitch(twelveToneScale[j], octave))/log(2)); 
+                // This prints pitch frequency
+                Serial.println(peak); 
                 }
                 
               }
@@ -318,15 +264,8 @@ void loop() {
       //Serial.println(peak);
       //delay(20);
 
+
   peak_last = peak;
+  //delay(200);
   }
-  
-  // Write the LED pins to low once we're done playing a note.
-
-
-  //Serial.print(twelveToneNames[j]);
-  
-
-  //delay(500);
-
 }
